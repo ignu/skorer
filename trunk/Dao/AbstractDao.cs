c62b6dab651595bf293f90b39201626145d4b5ec
ignu.smith <ignu.smith@6fdc5a3d-3341-0410-b760-d9515ec211ee>
2007-12-13 23:31:02 +0000

    initial commit

---
+++ trunk/Dao/AbstractDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+//namespace Dao
+//{
+//    public class GameDao : AbstractDao<GameDao>
+//    {
+//        private const string BASE_SQL = "SELECT ID, Name, MatchTitle FROM [Game]";
+//
+//
+////        protected override GameDao FromDataReader()
+////        {
+////            
+////        }
+//    }
+//
+//    public abstract class AbstractDao<T>
+//    {
+//        public int ID { get; set; }
+//
+////        private IList<T> MultipleFromSql(string sql)
+////        {
+////            
+////        }
+//
+//        protected abstract T FromDataReader();
+//        
+//
+//    }
+//}
