d271e461a7b9f65d1eb70f373cfb500342cda939
ignu.smith <ignu.smith@6fdc5a3d-3341-0410-b760-d9515ec211ee>
2008-04-15 20:39:46 +0000

    Match Controller

---
+++ Skorer.Tests/Controllers/MatchControllerTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using Skorer.Web.Controllers;
+using Skorer.DataAccess;
+using Moq;
+using Skorer.Core;
+using Castle.MonoRail.Framework.Test;
+using Castle.MonoRail.Framework;
+
+namespace Skorer.Tests.Controllers
+{
+    [TestFixture]
+    public class MatchControllerTests
+    {
+        MatchController _Controller;
+        Mock<IRepository<Match, int>> _MatchRepositoryMock;
+        Mock<IRepository<Game, int>> _GameRepositoryMock;
+        Mock<IRepository<Player, int>> _PlayerRepositoryMock;
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _MatchRepositoryMock = new Mock<IRepository<Match, int>>();
+            _GameRepositoryMock = new Mock<IRepository<Game, int>>();
+            _PlayerRepositoryMock = new Mock<IRepository<Player, int>>();
+            _Controller = new MatchController(_MatchRepositoryMock.Object, _GameRepositoryMock.Object, _PlayerRepositoryMock.Object);
+            _Controller.Contextualize(new MockEngineContext(), new ControllerContext());
+        }
+
+        [Test]
+        public void Can_Create_Match()
+        {
+            Game game = new Game { Name = "Politics" };
+            _GameRepositoryMock.Expect(g => g.GetById(1)).Returns(game);
+            Match newMatch = new Match { ID = 0, Game = game };
+            _GameRepositoryMock.Expect(g => g.GetAll()).Returns(new List<Game> { game });
+
+
+            _MatchRepositoryMock.Expect(m => m.Save(It.Is<Match>(ma => ma.Game.Name == "Politics"))).Returns(newMatch);
+
+            _Controller.Create(1);
+            Assert.AreEqual((_Controller.Context.Session["Match"] as Match).ID, newMatch.ID);
+        }
+
+        [Test]
+        public void Can_Add_Player()
+        {
+            Game game = new Game { Name = "Politics" };
+            Match newMatch = new Match { ID = 0, Game = game };
+            _Controller.Context.Session["Match"] = newMatch;
+
+            Player player = new Player { Name = "Tony" } ;
+            
+            _PlayerRepositoryMock.Expect(p => p.GetById(2)).Returns(player);
+            _PlayerRepositoryMock.Expect(p => p.Save(player)).Returns(player);
+            
+            _Controller.AddPlayers(2);
+            Assert.Greater((_Controller.Context.Session["Match"] as Match).Players.Count, 0);                        
+        }
+    }
+}
---
+++ Skorer.Web/Controllers/MatchController.cs
@@ -0,0 +1,71 @@
+﻿using Castle.MonoRail.Framework;
+using Skorer.DataAccess;
+using Skorer.Core;
+using System;
+
+namespace Skorer.Web.Controllers
+{
+    [Layout("default")]
+    public class MatchController : BaseController
+    {
+        IRepository<Match, int> _MatchRepository;
+        IRepository<Game, int> _GameRepository;
+        IRepository<Player, int> _PlayerRepository;
+
+        public MatchController(IRepository<Match, int> matchRepository, 
+            IRepository<Game, int> gameRepository,
+            IRepository<Player, int> playerRepository
+            )
+        {
+            _MatchRepository = matchRepository;
+            _GameRepository = gameRepository;
+            _PlayerRepository = playerRepository;
+        }
+
+
+        private void Start()
+        {
+            _MatchRepository.Save(_SessionMatch());            
+        }
+
+        private void _LoadGames()
+        {
+            PropertyBag["Games"] = _GameRepository.GetAll();
+        }
+        
+        public void Create()
+        {
+            _LoadGames();  
+        }
+        public void Create(int gameID)
+        {
+            _LoadGames();
+            Match match = new Match { Game = _GameRepository.GetById(gameID) };
+            _MatchRepository.Save(match);
+            Session["Match"] = match;
+            RedirectToAction("AddPlayers");
+        }
+
+        public void AddPlayers()
+        {
+            if (Session["Match"] == null)
+                throw new InvalidOperationException("There is no current Match");
+        }
+
+        private static Match _SessionMatch()
+        {
+            return ((Match)Session["Match"]);
+        }
+
+        public void AddPlayers(int playerID)
+        {
+            if (Session["Match"] == null)
+                throw new InvalidOperationException("There is no current Match");
+
+            Player playerToAdd = _PlayerRepository.GetById(playerID);
+
+            _SessionMatch().Players.Add(playerToAdd);
+                        
+        }
+    }
+}
